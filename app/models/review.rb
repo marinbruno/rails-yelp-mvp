b6c12bd240459de67b0be9dd0f0f52a848a12be1
Bruno <bruno_marin@hotmail.com>
2019-05-29 16:23:27 -0300

    Almost done

---
+++ app/models/review.rb
@@ -0,0 +1,5 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+  validates :content, presence: true, allow_blank: false
+  validates :rating, presence: true, numericality: { only_integer: true }, allow_blank: false, inclusion: { in: [0, 1, 2, 3, 4, 5] }
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+10.times do |restaurant|
+  restaurant_hash = {
+    name: Faker::Games::Dota.hero,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number_with_country_code,
+    category: ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample,
+  }
+  restaurant = Restaurant.create(restaurant_hash)
+end
